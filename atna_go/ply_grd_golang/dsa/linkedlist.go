90bf3ecb12102555b919eecbe155bd96af9446f2
sy-md <mdorsetti733@gmail.com>
2022-11-15 20:36:52 +0900

    update using new methods

--- atna_go/ply_grd_golang/dsa/linkedlist.go
+++ atna_go/ply_grd_golang/dsa/linkedlist.go
@@ -1,32 +1,59 @@
-type Node struct {
-	data interface{}
-	nx   *Node
+package main
+
+import (
+	f "fmt"
+)
+
+type node struct { // my nodes [1,[2,[...]]]
+	data int
+	nx   *node
 }
+type linkedlist struct{ head *node }
 
-type mylist struct {
-	head *Node
-}
+func (l *linkedlist) insert(num int) { // inserting node at end of linkedlist
+	nd := &node{data: num} //create a node
+	cur := l.head          //currenet node
 
-func (l *mylist) insert(val int) {
-	nd := &Node{data: val} // make node
-	p := l.head            //var for head
-
-	if l.head == nil { // check if head is nil
+	if l.head == nil { //if no head make a head
 		l.head = nd
-	} else { // if not nil
-		for p.nx != nil {
-			p = p.nx
+	} else {
+		for cur.nx != nil { //find the next avaiable spot for the node
+			cur = cur.nx //ever node go to your next node
 		}
-		p.nx = nd
+		cur.nx = nd //makes last nodes next the new node
 	}
 }
 
+func (l *linkedlist) add_end(val int) { // adding a node at the end of a linkedlist
+	nd := &node{ //make the node
+		data: val,
+	}
+	cur := l.head // repersent the first node
+	nd.nx = cur   // make the new node the new head
+	l.head = nd   // then ofically make the new node the head
+
+	f.Printf("\nheads next before: %v", l.head.nx.data)     //1
+	f.Printf("\nadd new node at head: %v \n", l.head.data)  //4
+	f.Printf("heads new next after: %v \n", l.head.nx.data) //1
+
+}
+
+func (l *linkedlist) display() { // displaying the linkedlist
+	cur := l.head //current node
+
+	for cur.nx != nil {
+		f.Printf("%v -->", cur.data)
+		cur = cur.nx
+	}
+	f.Printf("%v -->end", cur.data)
+}
+
 func main() {
-	kk := mylist{}
-	kk.insert(1)
-	kk.insert(2)
-	kk.insert(3)
-	f.Printf("%v", kk.head.data)
-	f.Printf("%v", kk.head.nx.data)
-	f.Printf("%v", kk.head.nx.nx.data)
+	ll := linkedlist{}
+	ll.insert(1)
+	ll.insert(2)
+	ll.insert(3)
+	ll.display() // 1,2,3
+	ll.add_end(4)
+	ll.display() // 4,1,2,3
 }
