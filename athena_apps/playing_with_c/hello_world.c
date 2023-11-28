c3d11716ebc7cb11876a5cc0c87dbf5870862c24
sy-md <mdorsetti733@gmail.com>
2023-11-28 14:42:58 -0700

    updte

--- athena_apps/playing_with_c/hello_world.c
+++ athena_apps/playing_with_c/hello_world.c
@@ -1,11 +1,24 @@
 #include <stdio.h>
 
-char greet[] = "Hello, world!";
+/*declarations functions above the main fucntions
+must match the defintinive functions so void void and char and the 
+char i think we should test the dict and arrays and what not*/
+//void test_function();
+
+
+//char greet[] = "Hello, world!";
+//char greet = "Hello, world!";
 
 int main(void)
 {
-    printf("%s\n", greet);
+	//test_function();
+	printf("hello world\n");
     return 0;
 }
 
+//void test_function() {
+//	printf("%s\n", greet);
+//	printf("testing");
+//
+//}
 
