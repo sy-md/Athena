34594711464e817530376baaaf69cff2a0ee843a
sy-md <73502678+sy-md@users.noreply.github.com>
2023-11-29 14:57:39 -0700

    Update hello_world.c
    so i found how to string concencate and a now lib to use string.h with that being said i always wonder what h extentsion have in side then... anyways we also made a new function

--- athena_apps/playing_with_c/hello_world.c
+++ athena_apps/playing_with_c/hello_world.c
@@ -1,11 +1,49 @@
+
 #include <stdio.h>
+#include <string.h>
 
+/*declarations functions above the main fucntions
+must match the defintinive functions so void void and char and the 
+char i think we should test the dict and arrays and what not*/
+;
+char name[] = "martell";
 char greet[] = "Hello, world!";
+//char greet = "Hello, world!"; this way doesnt work
 
-int main(void)
-{
-    printf("%s\n", greet);
-    return 0;
+// void functions used as a template for knowing what file is what and what they return ?
+void test_function();
+void secret_name()
+
+// main function
+int main(void) {
+	test_function();
+	printf("");
+	secret_name();
+	//printf("hello world\n");
+    	return 0;
 }
+
+// and these are defintive functions that run now sure how the layout is suppose to be
+void test_function() {
+	if (name == "martell"); {
+		printf("this a name thats works");
+	}
+	printf("%s\n", greet);
+	//printf("testing");
+
+}
+
+void secret_name() {
+	//int i;
+	for (i = 0; i < strlen(name); i++) {
+		i = i;
+	}
+	printf("your name is %s \n", name);
+	printf("your name has %i chartcers \n", i);
+
+}
+
+
+
 
 
