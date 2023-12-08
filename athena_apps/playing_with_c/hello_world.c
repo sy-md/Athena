453d859403818e0e926fe6c606e825792459bb93
sy-md <73502678+sy-md@users.noreply.github.com>
2023-12-08 08:47:34 -0700

    Update hello_world.c

--- athena_apps/playing_with_c/hello_world.c
+++ athena_apps/playing_with_c/hello_world.c
@@ -1,67 +1,28 @@
 #include <stdio.h>
 #include <string.h>
 
-<<<<<<< HEAD
 /*
 void functions used as a template for the compiler to know what to expect
 */
 char test_function();
 char secret_name();
 char add_test();
-=======
-/*declarations functions above the main fucntions
-must match the defintinive functions so void void and char and the 
-char i think we should test the dict and arrays and what not*/
->>>>>>> origin/TomorrowLand
+void WhoFreindsWithWho();
 
 //char greet = "Hello, world!"; this way doesnt work
 char name[] = "martell";
 char greet[] = "Hello, world!";
 
-<<<<<<< HEAD
 int main(void) {
+	//can i call a funtion from within a function? making my main function cleaner and only caling one function
 	test_function();
 	printf("\n");
 	add_test();
-	prtinf("\n");
+	printf("\n");
 	secret_name();
 	printf("\n");
 	printf("hello world\n");
     return 0;
-=======
-// void functions used as a template for knowing what file is what and what they return ?
-char test_function();
-char secret_name();
-char add_test();
-
-// main function
-int main(void) {
-	//test_function();
-	//printf("");
-	add_test();
-	//secret_name();
-	//printf("hello world\n");
-    	return 0;
-}
-
-// and these are defintive functions that run now sure how the layout is suppose to be
-char test_function() {
-	if (name == "martell"); {
-		printf("this a name thats works");
-	}
-	printf("%s\n", greet);
-	//printf("testing");
-}
-
-char secret_name() {
-	int i;
-	for (i = 0; i < strlen(name); i++) {
-		i = i;
-	}
-	printf("your name is %s \n", name);
-	printf("your name has %i chartcers \n", i);
-	
->>>>>>> origin/TomorrowLand
 }
 
 char test_function() {
@@ -89,9 +50,11 @@ char secret_name() {
 }
 
 char add_test() {
-	char* friends[5] = {"tim"};
-	//or
-	//char friends[0][5]; --> works on if your use snprintf()
+	/*
+		a function to test the array system in c... can i add to an array?
+		while using concatenation
+	*/
+	char* friends[5] = {"tim"}; // OR : --> char friends[0][5]; --> works on if your use snprintf()
 	printf("the old list of friends --> [ %s ] \n", friends[0]);
 	friends[1] = "wow";
 	printf("changeing %s to %s\n", friends[0], friends[1] );
@@ -99,32 +62,76 @@ char add_test() {
 	printf("the new list of friends --> [ %s ]", friends[2]);
 }
 
+void WhoFreindsWithWho() {
+	/*
+		creating a struct to hold data
+	*/
+	struct people {
+		char name[50]; // char* use string literal
+		int age;
+		char friends[2][50];
+	};
+	
+	struct people peeps[] = { {.name = "martell", .age = 21, .friends = "tim" }, {"tim", 21, "martell"} };
 
 
-/**
+	// what is the difference between the two?
 
-for loops 2 more
+	struct people p1 = {"martell", 21, "tim"}; 
+	struct people p2 = {"tim", 21, "martell"};
+	char* friends[2] = {p1[0].name, p1[2].friends};
 
-start using structs and arrays
+	/* 	
+		so why does it wont me to do it like this: 
+		isnt this creating another whole struct?
+	*/
+	 struct people myPeople[2]  = {
+		{"martell", 21, "tim"},
+		{"tim", 21, "martell"}
+	};
+
+	// but why cant i do this?
+
+	struct people p1 = {"martell", 21, "tim"}; 
+	struct people p2 = {"tim", 21, "martell"};
+	struct people friends[2] = {&p1, &p2};
+
+	printf("%s is %d years old and his friends is %s",p1.name, p1.age, p1.friends);
+	// or
+	//printf("%s is %d years old and his friends is %s",myPeople[0].name, myPeople[0].age, myPeople[0].friends);
+}
+
+
+/*
+
+next play with the for loops and while loops and u should have the basics isshhh
+
+lifetimes of variables
+	- static
+	- automatic
+	- allocated
+	- thread
+	- register
+ 
+  int myNumbers[4] = {25, 50, 75, 100};
+  int *ptr = myNumbers;
+  int i;
+
+  for (i = 0; i < 4; i++) {
+    printf("[Pointer Method] --> %d: i is %d\n", *(ptr + i), i);
+  }
+    // same as
+  for (i = 0; i < 4; i++) {
+    printf("[Array calling Method] --> %d: i is %d\n", myNumbers[i],i);
+  }
+  return 0;
+}
 
 
 
+ 
 
 */
-
-
-
-char add_test() {
-	char* friends[5] = {"tim"}; //works simply with ____[] = ___;
-	//or
-	//char friends[0][5]; // works on if your use snprintf()
-	printf("the old list of friends --> [ %s ] \n", friends[0]);
-	friends[1] = "wow";
-	printf("changeing %c to %c\n", friends[0], friends[1] );
-	friends[2] = "jimmy";
-	printf("the new list of friends --> [ %s ]", friends[2]);
-
-}
 
 
 
