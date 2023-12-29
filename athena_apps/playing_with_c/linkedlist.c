9d61994f66d0490f5293f8485c34e1ff69b97bdf
sy-md <73502678+sy-md@users.noreply.github.com>
2023-12-29 12:49:57 -0700

    Update linkedlist.c
    fixed the isssue on using the prev

--- athena_apps/playing_with_c/linkedlist.c
+++ athena_apps/playing_with_c/linkedlist.c
@@ -3,15 +3,11 @@
 
 int LinkListTest();
 
-int main(void) {
-	LinkListTest();
-    return 0;
-};
-
 struct node {
 	int val;
 	//int next;
 	struct node* next;
+    struct node* prev;
 };
 struct linkedlist {
 	//int head;
@@ -20,6 +16,13 @@ struct linkedlist {
 	struct node* head;
 	struct node* prev;
 };
+
+int main(void) {
+	LinkListTest();
+    return 0;
+};
+
+
 int LinkListTest() {
 	/* 
 		given a array of num 1,2,3 create a linklist
@@ -31,14 +34,16 @@ int LinkListTest() {
 	*/
 	struct linkedlist lst; // create a linkedlist
 	
-	struct node nd = {1};  // create a node
-	struct node ndd = {2};
+	struct node nd = {1, NULL};  // create a node
+	struct node ndd = {2, NULL};
 	
 	lst.head = &nd; // assign a node the a pos in the linkedlist
 	lst.head->next = &ndd;
+    lst.head->next->prev = &nd;
 	
 	printf("%d ->",lst.head->val);
 	printf("%d ->",lst.head->next->val);
+    printf("<- %d",lst.head->next->prev->val);
 
 };
 
