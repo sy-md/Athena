e4b5d8de2c6958c505366123b78be290a5fa0370
sy-md <73502678+sy-md@users.noreply.github.com>
2024-01-02 14:13:37 -0700

    first linklist
    the right to create a linkedlist

---
+++ athena_apps/playing_with_c/linkedlist.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h> // needed for using malloc()
+
+void cycle_linklist();
+
+struct node {
+  int val;
+  struct node *next; 
+};
+
+void cycle_linklist(struct node *p) {
+    while (p != NULL) {
+        printf("%d->", p->val);
+        p = p->next;
+    }
+};
+
+int main() {
+
+    /*
+        **initalize the creation of the nodes**
+
+        below we created the node
+        struct node *___ = NULL --> {_,NULL}
+        we assign it as ___
+        the malloc function is telling the machine to make room for
+        another node that will be taking up that sapce [64->8bit/ 32->4bits]
+    */
+    struct node *head;
+    struct node *one = malloc(sizeof(struct node));
+    struct node *two = malloc(sizeof(struct node));
+
+    // Assign value values
+    one->val = 1;
+    two->val = 2;
+
+    // Connect nodes
+    one->next = two;
+    two->next = NULL;
+
+    head = one;
+
+    cycle_linklist(head);
+
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
