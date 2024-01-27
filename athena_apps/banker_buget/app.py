16850a1fb79a88bd150a4287c30f6df346b6d806
sy-md <73502678+sy-md@users.noreply.github.com>
2024-01-27 12:54:54 -0700

    Update app.py
    had a thought for two graphs

--- athena_apps/banker_buget/app.py
+++ athena_apps/banker_buget/app.py
@@ -19,3 +19,9 @@ fig.add_trace(go.Scatter(x=df["x"], y=df["y"], mode='lines')) #the orange line
 
 fig.show()
 print(df)
+
+
+i should use two graphgs one for like the points of $600 and $300 so this will be twp points
+this  is also a 50% decrese to then then the other grpah will show the the point of 50% 
+then when the another point is added for example $300 + $150 then the next point on the perctenage grpah will
+be 25% so itll show 50% to 25% showing a negative trend line in percentages
