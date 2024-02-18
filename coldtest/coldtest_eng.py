8d4892b6d4fe3267c9115d6e6127dee75bcb56f0
sy-md <mdorsetti733@gmail.com>
2024-02-17 19:38:21 -0700

    im updating the coldtest i need to finshed this

--- coldtest/coldtest_eng.py
+++ coldtest/coldtest_eng.py
@@ -4,58 +4,8 @@ from plotly.subplots import make_subplots
 from dash import Dash, html, dcc
 import pandas as pd
 import pprint
-
-colors = {
-    "background": '#2e2b28',
-    "text": '#76c8c8'
-}
-
-file1 = "Cold_Testing_Without_internet.csv"
-file2 = "Cold_Testing_40C_W_Internet.csv"
-
-# Load the CSV data
-data = pd.read_csv(file1, sep=",")
-data2 = pd.read_csv(file2, sep=",")
-
-#the doctionary for the data
-
-bluetooth_temp = {
-    #data
-    '12Temp_bt_d1': data['12" Temp (BT)'],
-    '10Temp_bt_d1': data['10" Temp (BT)'], 
-    'ChamberTemp_bt_d1': data['Chamber Temp (BT)'],
-    #data2
-    '12Temp_bt_d2': data2['12" Temp (BT)'],
-    '10Temp_bt_d2': data2['10" Temp (BT)'],
-    'ChamberTemp_bt_d2': data2['Chamber Temp (BT)']
-}
-thermocouple_temp = {
-    #data
-    '12Temp_tc_d1': data['12" Temp (TC-4)'],
-    '10Temp_tc_d1': data['10" Temp (TC-3)'],
-    'ChamberTemp_tc_d1': data['Chamber Temp (TC-2)'],
-    #data2
-    '12Temp_tc_d2': data2['12" Temp (TC-4)'],
-    '10Temp_tc_d2': data2['10" Temp (TC-3)'],
-    'ChamberTemp_tc_d2': data2['Chamber Temp (TC-2)']
-}
-cpu_temp = {
-    #data
-    '12Temp_cpu_d1': data['12" Temp (CPU)'],
-    '10Temp_cpu_d1': data['10" Temp (CPU)'],
-    #data2
-    '12Temp_cpu_d2':data2['12" Temp (CPU)'],
-    '10Temp_cpu_d2': data2['10" Temp (CPU)'],
-}
-heater_current = {
-    #data
-    '12Current_d1': data['12" Current'],
-    '10Current_d1': data['10" Current'],
-    #data2
-    '12Current_d2': data2['12" Current'],
-    '10Current_d2': data2['10" Current'],
-}
-
+from coldtest import * 
+from coldtest import bluetooth_temp, thermocouple_temp
 fig = make_subplots(rows=4,cols=2)
 
 # row 1 with cols 1 bluetooth temp
@@ -154,6 +104,7 @@ fig.update_layout(
     font_color=colors['text']
 )
 
+
 # Create a Dash app
 app = Dash(__name__)
 
@@ -173,4 +124,3 @@ app.layout = html.Div(
 # Run the app
 if __name__ == '__main__':
     app.run_server(debug=True)
-
