84d6e3cdc6305ada871184857bdaed616e7e8755
sy-md <mdorsetti733@gmail.com>
2024-02-26 17:34:22 -0700

    nothing

--- coldtest/coldtest.py
+++ coldtest/coldtest.py
@@ -1,71 +1,32 @@
 import plotly.graph_objects as go
-import plotly.express as px
 from plotly.subplots import make_subplots
-from dash import Dash, html, dcc
-import pandas as pd
+from dash import dcc, html, Dash
 
-colors = {
-    "background": '#111111'
-}
-
-# Load the CSV data
-data = pd.read_csv('Testing.csv', sep=",")
-
-
-fig = make_subplots(rows=1,cols=2)
-
-
-
-fig.add_trace( 
-    go.Scatter(x=data['Time'], y=data['12Tempbt'], mode='lines'),
-    row=1, col=1)
-
-fig.add_trace( 
-    go.Scatter(x=data['Time'], y=data['10Tempbt'], mode='lines'),
-    row=1, col=1)
-
-fig.add_trace( 
-    go.Scatter(x=data['Time'], y=data['ChamberTempbt'], mode='lines'),
-    row=1, col=1)
-
-
-
-fig.add_trace( 
-    go.Scatter(x=data['Time'], y=data['12Temptc'], mode='lines'),
-    row=1, col=2)
-
-fig.add_trace( 
-    go.Scatter(x=data['Time'], y=data['10Temptc'], mode='lines'),
-    row=1, col=2)
-
-fig.add_trace( 
-    go.Scatter(x=data['Time'], y=data['ChamberTemptc'], mode='lines'),
-    row=1, col=2)
-
-
-fig.update_layout(
-    #height=600,width=900,
-    #plot_bgcolor=colors['background'],
-    #paper_bgcolor=colors['background'],
-)
-
-# Create a Dash app
 app = Dash(__name__)
 
-# Define the layout
-app.layout = html.Div(
-    #style={'backgroundColor': colors['background']},
+fig = make_subplots(rows=1, cols=2)
 
-    children=[
-        html.H1(children="fdhdfkjsdhf"),
+trace1 = fig.add_trace(go.Scatter(
+    x=[1, 2, 3],
+    y=[4, 5, 6]
+), row = 1, col = 1)
+
+trace2 = fig.add_trace(go.Scatter(
+    x=[20, 30, 40],
+    y=[50, 60, 70],
+), row = 1, col = 2)
+
+data: list = [trace1, trace2]
+
+app.layout = html.Div(
+
+    children=[html.H1("Hello Dash"),
 
     dcc.Graph(
-        id="example1",
-       figure=fig
+        id='example-graph',
+        figure=fig
     )
 ])
 
-# Run the app
 if __name__ == '__main__':
     app.run_server(debug=True)
-
--- test.py
+++ test.py
@@ -1,3 +1,51 @@
-"create a hello world app in python but in goland sturcture"
+from typing import Optional 
 
+# Definition for singly-linked list.
+class ListNode:
+     def __init__(self, val=0, next=None):
+         self.val = val
+         self.next = next
+
+class Solution:
+    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        """   
+            cur = l1
+            curr = l2
+            while cur.next is not None:
+                x = cur.val
+                cur = cur.next
+                y = curr.vals
+            print(cur.val + curr.val)
+        """
+
+        # after learning the dummy head tech this how we solve this now
+        dum = cur = ListNode(0)
+
+        # if both list dont have a next node return
+        if l1.next is None and l2.next is None:
+            return dum
+
+        while l1 and l2:
+            nd = l1.val + l2.val
+            if nd > 9:
+                nw = nd % 10
+                nw = ListNode(nd)
+                l1.next.val + nw.val
+
+                nd1 = nd - nw.val
+                cur.next = ListNode(nd1) 
+
+            else:
+                cur.next = nd
+                l1 = l1.next
+                l2 = l2.next
+        print(cur)
+
+
+if __name__ == "__main__":
+    s = Solution()
+    s.addTwoNumbers([2,4,3], [5,6,4])
+
+
+    
 
